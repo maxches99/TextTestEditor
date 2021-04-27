19f59dfadf14ae3e7ece33ac47bf0c996839d410
Max Chesnikov <maxches99@gmail.com>
2021-04-27 15:41:00 +0300

    Initial Commit

---
+++ TextTestEditor/TextTestEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextTestEditorApp.swift
+//  TextTestEditor
+//
+//  Created by Максим Чесников on 27.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct TextTestEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
